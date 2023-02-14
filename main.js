6ecf258e431376f5792e178c4ed6984cfbc677dc
lauramorenochico <laura.moreno.chico@gmail.com>
2023-02-14 09:23:29 +0100

    add structure HTML

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log ('holisss'); //bien enlzadado
+
+//constantes.
+//evento del botón. 
+//función ( acceder al input y mostarrlo en la consola)
+// función (comparar números y dar pistas)
+//actualizar el contador 
